eaf3a9f0198b38c729e84252ab0196a4ef384ef2
danielburke <danielburke1206@gmail.com>
2023-03-22 07:52:24 -0400

    added playercount setup

--- Game.java
+++ Game.java
@@ -9,6 +9,7 @@ public class Game {
     private static int playerCount;
     private static ArrayList<Integer> playerScores = new ArrayList<>();
     private static int round;
+    private static String[] playerNames = new String[playerCount];
 
     /**
      * Creates hand for each player
@@ -16,8 +17,21 @@ public class Game {
      * each item in the array is one player's hand, stored in an ArraList<Card> object
      */
     ArrayList<Card>[] playerHands = new ArrayList[playerCount];
-    for(int i = 0; i < playerHands.length; i++){
+    for(int i = 0; i<playerHands.length; i++){
         playerHands[i] = new ArrayList<Card>(7);
     }
-}
 
+    /* getters and setters */
+    public static int setPlayerCount(int playerCount) {
+        Game.playerCount = playerCount;
+        return playerCount;
+    }
+
+    public static int getPlayerCount() {
+        return playerCount;
+    }
+
+    public static void setPlayerNames(int index, String name) {
+        Game.playerNames[index] = name;
+    }.
+}
