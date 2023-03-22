d4afcde5d22852fbe3ce896f517099f711285325
danielburke <danielburke1206@gmail.com>
2023-03-21 23:08:28 -0400

    added user confirmation

--- Game.java
+++ Game.java
@@ -9,4 +9,15 @@ public class Game {
     private static int playerCount;
     private static ArrayList<Integer> playerScores = new ArrayList<>();
     private static int round;
+
+    /**
+     * Creates hand for each player
+     * playerHands is the Array which stores each player's hand in the game
+     * each item in the array is one player's hand, stored in an ArraList<Card> object
+     */
+    ArrayList<Card>[] playerHands = new ArrayList[playerCount];
+    for(int i = 0; i < playerHands.length; i++){
+        playerHands[i] = new ArrayList<Card>(7);
+    }
 }
+
